df4c23e43a12822eca014d051821157481f89ce3
Patro-ss <patross937@gmail.com>
2020-10-25 18:53:02 +0100

    Added new labels after click button, change gui colors

--- playergui.py
+++ playergui.py
@@ -15,13 +15,14 @@ playlist = os.listdir()
 
 choice = 1
 app = Tk()
-icon = PhotoImage(file='Your file')
+icon = PhotoImage(file='/home/patro-ss/Documents/Python/index.ico')
 app.tk.call('wm', 'iconphoto', app._w, icon)
 app.title("Player")
 app.geometry('800x600')
+app.configure(bg="#383838")
 scrollbar = Scrollbar(app)
 scrollbar.pack(side = RIGHT, fill = Y)
-filename = Listbox(app, highlightcolor = "black", fg = "white", bd = '20px', height = '20', width = '40', yscrollcommand = scrollbar.set, selectmode = SINGLE)
+filename = Listbox(app, highlightcolor = "black",bg = "#303030" ,fg = "white", bd = '20px', height = '20', width = '40', yscrollcommand = scrollbar.set, selectmode = SINGLE)
 filename.pack()
 
 for i in range(0):
@@ -34,35 +35,39 @@ for music_file in playlist:
 	filename.insert(position, music_file)
 	position = position + 1
 
-def play_music():
-    pygame.mixer.music.load(filename.get(ACTIVE))
-    var.set(filename.get(ACTIVE))
-    pygame.mixer.music.play()	
-playbutton = Button(app, text="Play ", command=play_music)
-playbutton.pack(fill="x")
-
 var = StringVar()
 musictitle = Label(app, textvariable=var)
 musictitle.pack() 
 
-def pause_music():
-	pygame.mixer.music.pause()
-playbutton = Button(app, text="Stop ", command=pause_music)
+def play_music():
+    pygame.mixer.music.load(filename.get(ACTIVE))
+    var.set(filename.get(ACTIVE))
+    pygame.mixer.music.play()	
+playbutton = Button(app, text="Play ", bg="#383838",command=play_music)
 playbutton.pack(fill="x")
 
+def pause_music():
+    pygame.mixer.music.pause()
+    var.set("Music is stopped")
+playbutton = Button(app, text="Stop ", bg="#383838", command=pause_music)
+playbutton.pack(fill="x") 
+
 def unpause_music():
-	pygame.mixer.music.unpause()
-playbutton = Button(app, text="Resume ", command=unpause_music)
+    pygame.mixer.music.unpause()
+    var.set("Music is resumed: " + filename.get(ACTIVE))
+playbutton = Button(app, text="Resume ", bg="#383838", command=unpause_music)
 playbutton.pack(fill="x")
 
 def mute_music():
-	pygame.mixer.music.set_volume(0.0)
-playbutton = Button(app, text="Mute ", command=mute_music)
+    pygame.mixer.music.set_volume(0.0)
+    var.set("Music is muted: " + filename.get(ACTIVE))
+playbutton = Button(app, text="Mute ", bg="#383838", command=mute_music)
 playbutton.pack(fill="x")
 
 def unmute_music():
-	pygame.mixer.music.set_volume(1.0)
-playbutton = Button(app, text="Unmute ", command=unmute_music)
+    pygame.mixer.music.set_volume(1.0)
+    var.set("Music is unmuted: " + filename.get(ACTIVE))
+playbutton = Button(app, text="Unmute ", bg="#383838", command=unmute_music)
 playbutton.pack(fill="x")
 
 def random_music():
@@ -70,10 +75,12 @@ def random_music():
     pygame.mixer.music.load(filename)
     var.set(filename)
     pygame.mixer.music.play()
-playbutton = Button(app, text="Play random music ", command=random_music)
+playbutton = Button(app, text="Play random music ", bg="#383838", command=random_music)
 playbutton.pack(fill="x")
 
-"""
+app.mainloop()	
+
+'''
 Currently, autoplay is not working
 def autoplay_music(): 
     autoplay = False
@@ -94,5 +101,6 @@ def autoplay_off_music():
         pygame.mixer.music.pause()
 playbutton = Button(app, text="Autoplay off", command=autoplay_off_music)
 playbutton.pack(fill="x")
-"""
-app.mainloop()	
+'''
+
+
